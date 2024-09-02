c665f6fafadd704b19eb26b87540a1df51472e05
sherlock221B1 <gitacc4467@gmail.com>
2024-09-02 16:06:26 +0630

    epi-31-update

--- epi-31-multi-select-server-action/src/app/backoffice/menuss/page.tsx
+++ epi-31-multi-select-server-action/src/app/backoffice/menuss/page.tsx
@@ -107,7 +107,7 @@ export default async function MenusPage() {
         }}
       >
         {menus.map((menu) => {
-          return <MenuCard menu={menu} />;
+          return <MenuCard key={menu.id} menu={menu} />;
         })}
       </Box>
     </>
